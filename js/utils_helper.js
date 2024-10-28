56b7de73068758d61fa8a1562c2b1b2ab3725091
jonathan <jonathan_walden@hotmail.com>
2024-10-27 19:09:03 -0700

    allow caching of geojson

--- js/utils_helper.js
+++ js/utils_helper.js
@@ -1,6 +1,6 @@
 async function getJson(url) {
 	try {
-		const response = await fetch(url, {cache: 'no-cache'}); // https://hacks.mozilla.org/2016/03/referrer-and-cache-control-apis-for-fetch/
+		const response = await fetch(url); // {cache: 'no-cache'} https://hacks.mozilla.org/2016/03/referrer-and-cache-control-apis-for-fetch/
 		if (!response.ok) {
 			throw new Error(`Response status: ${response.status}`);
 		}
